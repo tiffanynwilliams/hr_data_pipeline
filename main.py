7371bc23c50d99c836683d59515c08c64d64c218
tiffany w. <91567004+tiffanynwilliams@users.noreply.github.com>
2026-03-24 20:28:14 -0400

    imported main.py as module as clean, used clean in place of cleaned_heartrate_data()

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-def clean_heartrate_data(data: list) -> tuple:
-    """
-    Clean raw heart-rate data by removing malformed or impossible values.
-    """
-    # cleaned_list will contain the stripped_heartrate values
-    # removed_values is a counter for how many non-digit characters in the data
+import data_cleaning as clean
+# def clean_heartrate_data(data: list) -> tuple:
+#     """
+#     Clean raw heart-rate data by removing malformed or impossible values.
+#     """
+#     # cleaned_list will contain the stripped_heartrate values
+#     # removed_values is a counter for how many non-digit characters in the data
 
-    cleaned_list = []
-    removed_val = 0 
+#     cleaned_list = []
+#     removed_val = 0 
     
-    # stripped_heartrate will have its integer-type heart rate values placed inside the cleaned_list
-    # return cleaned_list and removed_values back to run()
-    for heartrate in data:
-        stripped_heartrate = heartrate.strip()
-        if stripped_heartrate.isdigit() is True:
-            stripped_heartrate = (int(stripped_heartrate))
-            cleaned_list.append(stripped_heartrate)
-        else: 
-            # 
-            removed_val = removed_val + 1     
+#     # stripped_heartrate will have its integer-type heart rate values placed inside the cleaned_list
+#     # return cleaned_list and removed_values back to run()
+#     for heartrate in data:
+#         stripped_heartrate = heartrate.strip()
+#         if stripped_heartrate.isdigit() is True:
+#             stripped_heartrate = (int(stripped_heartrate))
+#             cleaned_list.append(stripped_heartrate)
+#         else: 
+#             # 
+#             removed_val = removed_val + 1     
                            
-    return (cleaned_list, removed_val)
+#     return (cleaned_list, removed_val)
 
-    pass
+#     pass
 
 
 def average(data: list) -> float:
@@ -123,7 +124,7 @@ def run(file: str):
     data = file_object.readlines()
     
     # Use `clean_heartrate_data` to clean the data and remove invalid entries
-    cleaned_list, removed_val = clean_heartrate_data(data)  
+    cleaned_list, removed_val = clean(data)  
     
     # calculate the average, median, and range of this file using the functions you've wrote
     average_heart_rate_data = average(cleaned_list)
