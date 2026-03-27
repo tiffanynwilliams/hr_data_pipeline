d0263fb2d093c46e52cced3f87f543f1a4841c6f
tiffany w. <91567004+tiffanynwilliams@users.noreply.github.com>
2026-03-27 02:20:27 -0400

    implemted standard deviation from variance_result, and generated visualisation of heart rate data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_cleaning import clean_heartrate_data
 from statistical import average, median, range
-from statistics import variance
+import math
+import matplotlib.pyplot as plt 
 
 # def clean_heartrate_data(data: list) -> tuple:
 #     """
@@ -103,9 +104,41 @@ from statistics import variance
 
 def variance(data: list) -> float:
     mean = average(data)
-    print(mean)
-    return mean
+    
+    squared_sums = 0
 
+    for heartrate in data:
+        heartrate -= mean
+        squared_vals = heartrate ** 2
+        squared_sums += squared_vals
+    
+    variance_result = round(squared_sums / (len(data)),2)
+
+    standard_dev = round(math.sqrt(variance_result), 2)
+
+    return (variance_result, standard_dev)
+
+def line_plot(data: list):
+    """
+    Create a visualisation using the cleaned_heartrate_data's values,
+    labelling the graph adaquetly.
+
+    Args:
+        cleaned_heartrate_data, the cleaned data list.
+    
+    Returns:
+        None, should save an image to images/ folder
+    """
+    fig, ax = plt.subplots()
+
+    ax.plot(data, color ='hotpink')
+    ax.set_title("Heart Rate Data, Individual in 30s")
+    ax.set_xlabel("Minutes")
+    ax.set_ylabel("Heart Rate (bpm)")
+    # save the image to the correct folder
+    fig.savefig("images/heart_rate_graph.png")
+    # best practice, helps with memory
+    plt.close()
 
 
 def run(file: str):
@@ -128,7 +161,7 @@ def run(file: str):
     
     # Use `clean_heartrate_data` to clean the data and remove invalid entries
     cleaned_list, removed_val = clean_heartrate_data(data)  
-    
+
     # calculate the average, median, and range of this file using the functions you've wrote
     average_heart_rate_data = average(cleaned_list)
     print("Average:", average_heart_rate_data, "bpm")   
@@ -139,16 +172,23 @@ def run(file: str):
     range_heart_rate_data = range(cleaned_list)
     print("Range:", range_heart_rate_data, "bpm")
 
+
+    # unpack the returned variance_result and standard_dev 
+  
+    variance_heart_rate_data, standard_dev = variance(cleaned_list)
+    print("Variance:", variance_heart_rate_data)
+
+    print ("Standard Deviation:", standard_dev)
+ 
+    # print a counter of removed values
     print("Removed Values:", removed_val, "heartrate(s)")
-
-    # printing out variance()
-    # variance_heart_rate_data = variance(cleaned_list)
-    variance_heart_rate_data = variance(cleaned_list)
-    print("VARIANCE:", variance_heart_rate_data)
-    # print("Variance:", round(variance_heart_rate_data, 2))
-
+ 
     # print out your data quality measure to the console
     print("Cleaned Heartrate Values:", cleaned_list, "\n")
+
+    # call the line_plot() to save an image of the heart rate graph in images/ folder
+    line_plot(cleaned_list)
+    
     
     #close() to stop reading the files
     file_object.close()
